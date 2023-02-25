70b6fea3690298c88bc4404636afef940313c267
Owen Zonneveld <owenzonneveld@gmail.com>
2023-02-25 10:22:18 -0600

    Even more Database filters!

--- static/assets/js/res/database_parsing/queries.js
+++ static/assets/js/res/database_parsing/queries.js
@@ -1,50 +1,30 @@
 
+database = require("../database.json")
 
-database = 
-{
-    "recipes" : [
-        {
-            "name" : "Pancakes",
-            "ingredients" : [
-                "Flower",
-                "Egg",
-                "Milk",
-                "Butter",
-                "Baking powder",
-                "Sugar"
-            ],
-            "prep_time" : 10,
-            "cooking_time" : 5,
-            "difficulty" : 2
-        },
-        {
-            "name" : "French Toast",
-            "ingredients" : [
-                "Flower",
-                "Egg",
-                "Milk",
-                "Butter",
-                "Baking powder"
-            ],
-            "prep_time" : 15,
-            "cooking_time" : 10,
-            "difficulty" : 2
-        }
-    ]
-};
 
-function get_aproximate_price(ingredient) {
-    return Math.random() * 100;
-}
- 
 // Gets all the recipes from the 
 function get_all_recipes() {
     return database.recipes;
 }
 
-// Gets the cost of a recipe
-function get_recipe_cost(recipe) {
-    return 10;
+// Gets all the authors in the Database
+function get_all_authors() {
+    return database.authors;
+}
+
+// Gets all the followed authors
+function get_followed_authors() {
+    return get_all_authors().filter(author => author.followed)
+}
+
+// Gets a all authors with this name
+function get_authors_with_name(author_name) {
+    return get_all_authors().filter(author => author.name.includes(author_name))
+}
+
+// Gets the approximate price of an ingredient
+function get_aproximate_price(ingredient) {
+    return Math.random() * 100; 
 }
 
 // Gets the total time to cookie a recipe
@@ -58,6 +38,11 @@ function get_approximate_recipe_cost(recipe) {
         .map(ing => { 
             return get_aproximate_price(ing);
         }).reduce((x, y) => { return x + y }, 0)
+}
+
+// Filters for favourited recipes
+function filter_favourited_recipes(recipes) {
+    return recipes.filter(recipe => recipe.favourited);
 }
 
 // Filters out the recipes for the time range
@@ -84,6 +69,27 @@ function filter_name(recipes, name) {
 
         return valid;
     });
+}
+
+// Filters the items based off followed authors
+function filter_followed_authors(recipes) {
+    followed = get_followed_authors();
+    followed_names = followed.map(follow => follow.name)
+
+    return recipes.filter(recipe => {
+        return followed_names.includes(recipe.author);
+    })
+}
+
+
+// Filters for authors with the name
+function filter_authors_with_name(recipes, author_name) {
+    authors = get_authors_with_name(author_name);
+    author_names = authors.map(author => author.name)
+
+    return recipes.filter(recipe => {
+        return author_names.includes(recipe.author);
+    })
 }
 
 // Filters recipes that have ALL of these ingredients
@@ -115,11 +121,16 @@ function recipe_filter(time_min, time_max, difficulty_min, difficulty_max, searc
     return recipes;
 }
 
+
 // Tests the database
 function test_db() {
     console.log(recipe_filter(10, 30, 1, 5, "pan", ["Egg"]));
     
     console.log(get_approximate_recipe_cost(get_all_recipes()[0]));
+    console.log(filter_favourited_recipes(get_all_recipes()));
+
+    console.log(filter_followed_authors(get_all_recipes()));
+    console.log(filter_authors_with_name(get_all_recipes(), "Owen"));
 }
 
 
