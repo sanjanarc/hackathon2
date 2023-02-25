376c520d4c05b61de7580297e31a6ab81ced7454
Carlo Garcia <garciac4@myumanitoba.ca>
2023-02-25 16:33:35 -0600

    initial search progress

--- static/assets/js/results_page.js
+++ static/assets/js/results_page.js
@@ -1,3 +1,22 @@
 function createResult(){
-    
+    var parent = document.getElementById("results");
+    var item = document.createElement("div");
+    item.id = "result";
+
+    var img = document.createElement("img");
+    img.src = "../static/assets/img/food1.jpg"; //change source of img
+    img.className = "result_img";
+
+    var name = document.createElement("div");
+    name.textContent = "result";
+
+    item.appendChild(img);
+    item.appendChild(name);
+    parent.appendChild(item);
+}
+
+window.onload = () =>{
+    for(i = 0; i < 10; i++){
+        createResult();
+    }
 }
