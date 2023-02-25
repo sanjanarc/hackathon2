13de0d6e961f76cd416fb82e608024d68fab94d6
Owen Zonneveld <owenzonneveld@gmail.com>
2023-02-25 09:18:11 -0600

    More Database filters

--- static/assets/js/res/database_parsing/queries.js
+++ static/assets/js/res/database_parsing/queries.js
@@ -13,7 +13,23 @@ database =
                 "Baking powder",
                 "Sugar"
             ],
-            "prep_time" : 10
+            "prep_time" : 10,
+            "cooking_time" : 5,
+            "difficulty" : 2
+        },
+        {
+            "name" : "French Toast",
+            "ingredients" : [
+                "Flower",
+                "Egg",
+                "Milk",
+                "Butter",
+                "Baking powder",
+                "Sugar"
+            ],
+            "prep_time" : 15,
+            "cooking_time" : 10,
+            "difficulty" : 2
         }
     ]
 };
@@ -23,10 +39,26 @@ function get_all_recipes() {
     return database.recipes;
 }
 
+// Gets the cost of a recipe
+function get_recipe_cost(recipe) {
+    return 10;
+}
+
+function get_recipe_total_time(recipe) {
+    return recipe.prep_time + recipe.cooking_time;
+}
 
 // Filters out the recipes for the time range
 function filter_time_range(recipes, min, max) {
-    return recipes.filter(recipe => { return recipe.prep_time >= min && recipe.prep_time <= max; });
+    return recipes.filter(recipe => { 
+        const total_time = get_recipe_total_time(recipe);
+        return total_time >= min && total_time <= max; 
+    });
+}
+
+// Filters out the recipes for the difficulty range
+function filter_difficulty_range(recipes, min, max) {
+    return recipes.filter(recipe => { return recipe.difficulty >= min && recipe.difficulty <= max; });
 }
 
 // Filters for the name
@@ -42,10 +74,22 @@ function filter_name(recipes, name) {
     });
 }
 
+// Filters out recipes that don't contain these ingrediants
+function filter_ingredients_exact(recipes, ingredients) {
+    return recipes.filter(recipe => {
+        return recipe.ingredients.every(ing => {
+            return ingredients.includes(ing);
+        });
+    });
+}
+
+// Tests the database
 function test_db() {
     console.log(filter_time_range(get_all_recipes(), 5, 15));
-    console.log(filter_name(get_all_recipes(), "pan"));
+    console.log(filter_name(get_all_recipes(), "to"));
+    console.log(filter_ingredients_exact(get_all_recipes(), ["Egg"]));
 }
+
 
 
 test_db();
